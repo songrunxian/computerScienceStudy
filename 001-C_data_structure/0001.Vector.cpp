81058d786164aa5b0d69d9ff0ff520d815c8a2ce
David_song <13359850710@163.com>
2023-09-22 21:44:39 +0800

    23.9.22

--- 001-C_data_structure/0001.Vector.cpp
+++ 001-C_data_structure/0001.Vector.cpp
@@ -21,10 +21,28 @@ void clear(Vector *vec){
 	return ;
 }
 
+int expand(Vector *vec) {
+	int extr_size = vec->size;
+	int *p;
+	while(extr_size) {
+		//这样是防止内存泄漏，要理解realloc的原理
+		p = (int *)realloc(vec->data, sizeof(int)*(vec->size + extr_size));
+		if(p) break;
+		extr_size /= 2;
+	}
+	if(p == NULL) return 0;
+	vec->data = p;
+	vec->size += extr_size;
+	return 1;
+}
+
 int insert(Vector *vec, int ind, int val) {
 	if(vec == NULL) return 0;
 	if(ind < 0 || ind > vec->length) return 0;
-	if(vec->length == vec->size) return 0;
+	if(vec->length == vec->size) {
+		if(!expand(vec)) return 0;
+		printf("expand successfully ! size = %d\n", vec-> size);
+	};
 	for(int i = vec->length; i > ind; i--) {
 		vec->data[i] = vec->data[i-1];
 	}
@@ -56,17 +74,20 @@ void output(Vector *vec) {
 int main() {
 	srand(time(0));
 	#define max_op 10
-	Vector *vec = init(max_op);
+	Vector *vec = init(3);
 	int op, ind, val;
 	for(int i = 0; i < max_op; i++) {
 		op = rand() % 2;	//设计了2种操作，那就%2，0是一种操作，1是另外一种操作
-		ind = rand() %(vec->length + 1);	// + 1 是因为，最开始length 是0
+		//ind = rand() %(vec->length + 1);	// + 1 是因为，最开始length 是0,这样生成的永远是正确的索引，调试要求是生成真正的随机数
+		ind = rand() %(vec->length + 3) -1; //这样可以生成负数索引
 		val = rand() % 100;
 		switch(op) {
-			case 0: {
+			case 0: 
+			case 1: 
+			case 2: {
 				printf("insert %d at %d to Vector = %d\n", val, ind, insert(vec, ind, val));
 			} break;
-			case 1: {
+			case 3: {
 				printf("erase item at %d from Vector = %d\n", ind, erase(vec, ind));
 			} break;
 		}
