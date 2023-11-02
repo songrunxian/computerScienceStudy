ebd9f565a6e757cc5ce247c69e7ee3a75fe53215
Song Runxian <13359850710@163.com>
2023-11-02 21:07:17 +0800

    2023.11.2

---
+++ 001-C_data_structure/Queue_Stack/HZOJ_372_monotonic_stack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cmath>
+using namespace std;
+#define MAX_N 500000
+int a[MAX_N + 5], b[MAX_N + b];
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i++) cin >> a[i];
+	for(int i = 0; i < n; i++) cin >> b[i];
+	int p = 1, top = 0, top2 = 0;
+	while(p < n) {
+		while(top1 != -1 && a[p] <= a[top1]) --top1;
+		while(top2 != -1 && b[p] <= b[top2]) --top2;
+		if(top1 - top2) break;
+		a[++top1] = a[p];
+		b[++top2] = b[p];
+		p++;
+	}
+	cout << p << endl;
+	return 0;
+}
